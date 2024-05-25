64443be429507cc420dd8a5ca3ce9dc38fb1ccaf
Nien-Ying Chou <wolfzxcv@gmail.com>
2024-05-25 21:21:27 +0200

    feat(dialogflow): integrate with dialogflow

---
+++ src/pages/api/webhook.ts
@@ -0,0 +1,71 @@
+import info from '@/utils/extra-information';
+import { openai } from '@/utils/openai';
+import { ChatCompletionMessage, ChatCompletionRole } from 'openai/resources';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export type IChatGPTMessage = {
+  role: ChatCompletionRole;
+  content: string | null;
+};
+
+const chatHistory: IChatGPTMessage[] = [...info];
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    try {
+      const lastQuestion: IChatGPTMessage = {
+        role: 'user',
+        // Input text from Dialogflow
+        content: req.body.queryResult.queryText
+      };
+
+      const messages = [...chatHistory, lastQuestion];
+
+      const chat = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        //@ts-ignore
+        messages
+      });
+
+      const lastAnswer: ChatCompletionMessage = chat.choices[0].message;
+
+      // update history
+      chatHistory.push(lastQuestion);
+      chatHistory.push(lastAnswer);
+
+      const result = lastAnswer.content;
+
+      const toPlatforms = {
+        google: {
+          expectUserResponse: true,
+          richResponse: {
+            items: [
+              {
+                simpleResponse: {
+                  textToSpeech: result
+                }
+              }
+            ]
+          }
+        }
+      };
+
+      const output = {
+        payload: toPlatforms,
+        fulfillmentText: result,
+        speech: result,
+        displayText: result,
+        source: ''
+      };
+      return res.json(output);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log('Please use only POST method');
+  }
+}
